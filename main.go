1db1e830831a17ebecc474359f0de351845dc6be
Marcus Weiner <marcus.weiner@gmail.com>
2018-11-19 22:11:13 +0100

    Format according to Go conventions

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"github.com/ajstarks/svgo"
-	"os"
 	"math"
+	"os"
+
+	"github.com/ajstarks/svgo"
 )
 
 type Point struct {
@@ -13,6 +14,7 @@ type Point struct {
 
 type Hexagon struct {
 	A, B, C, D, E, F Point
+
 	M Point
 	R int
 }
@@ -21,30 +23,30 @@ func (h *Hexagon) calc() {
 	// dot above
 	h.A.X = h.M.X
 	h.A.Y = h.M.Y - h.R
-	
+
 	// dot upper right
-	h.B.X = h.M.X + int(math.Round(float64(h.R) * math.Cos(math.Pi / 6)))
-	h.B.Y = h.M.Y - int(math.Round(float64(h.R) * math.Sin(math.Pi / 6)))
-	
+	h.B.X = h.M.X + int(math.Round(float64(h.R)*math.Cos(math.Pi/6)))
+	h.B.Y = h.M.Y - int(math.Round(float64(h.R)*math.Sin(math.Pi/6)))
+
 	// dot lower right
-	h.C.X = h.M.X + int(math.Round(float64(h.R) * math.Cos(math.Pi / 6)))
-	h.C.Y = h.M.Y + int(math.Round(float64(h.R) * math.Sin(math.Pi / 6)))
-	
+	h.C.X = h.M.X + int(math.Round(float64(h.R)*math.Cos(math.Pi/6)))
+	h.C.Y = h.M.Y + int(math.Round(float64(h.R)*math.Sin(math.Pi/6)))
+
 	// dot below
 	h.D.X = h.M.X
 	h.D.Y = h.M.Y + h.R
-	
+
 	// dot lower left
-	h.E.X = h.M.X - int(math.Round(float64(h.R) * math.Cos(math.Pi / 6)))
-	h.E.Y = h.M.Y + int(math.Round(float64(h.R) * math.Sin(math.Pi / 6)))
-	
+	h.E.X = h.M.X - int(math.Round(float64(h.R)*math.Cos(math.Pi/6)))
+	h.E.Y = h.M.Y + int(math.Round(float64(h.R)*math.Sin(math.Pi/6)))
+
 	// dot upper left
-	h.F.X = h.M.X - int(math.Round(float64(h.R) * math.Cos(math.Pi / 6)))
-	h.F.Y = h.M.Y - int(math.Round(float64(h.R) * math.Sin(math.Pi / 6)))
+	h.F.X = h.M.X - int(math.Round(float64(h.R)*math.Cos(math.Pi/6)))
+	h.F.Y = h.M.Y - int(math.Round(float64(h.R)*math.Sin(math.Pi/6)))
 }
 
 func (h Hexagon) sliceX() []int {
-	return []int {
+	return []int{
 		h.A.X,
 		h.B.X,
 		h.C.X,
@@ -55,7 +57,7 @@ func (h Hexagon) sliceX() []int {
 }
 
 func (h Hexagon) sliceY() []int {
-	return []int {
+	return []int{
 		h.A.Y,
 		h.B.Y,
 		h.C.Y,
@@ -78,7 +80,7 @@ func draw_mupliple_hexagons(canvas *svg.SVG, M Point, rounds int, radius int) {
 		M: M,
 		R: radius,
 	}
-	hexagon_width := float64(first_h.R) * 2 * math.Cos(math.Pi / 6)
+	hexagon_width := float64(first_h.R) * 2 * math.Cos(math.Pi/6)
 	first_h.calc()
 	first_h.draw(canvas, grays[0])
 	for c := 1; c < rounds; c++ {
@@ -97,14 +99,14 @@ func draw_mupliple_hexagons(canvas *svg.SVG, M Point, rounds int, radius int) {
 		for i := 1; i < count_hex; i++ {
 			h = Hexagon{
 				M: Point{
-					X: h.M.X + int(hexagon_width * math.Cos(deg)),
-					Y: h.M.Y + int(hexagon_width * math.Sin(deg)),
+					X: h.M.X + int(hexagon_width*math.Cos(deg)),
+					Y: h.M.Y + int(hexagon_width*math.Sin(deg)),
 				},
 				R: h.R,
 			}
 			h.calc()
 			h.draw(canvas, grays[i%4])
-			if i % c == 0 {
+			if i%c == 0 {
 				deg += math.Pi / 3 // 60°
 			}
 		}
