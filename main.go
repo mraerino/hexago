47d5640e69daaaa5ec0cd1bfbf8a9056d0687698
Marcus Weiner <marcus.weiner@gmail.com>
2018-11-20 01:07:24 +0100

    Implement neighbor collision detection
    WARNING: not ready for production. Infinite loops. You have been warned!

--- main.go
+++ main.go
@@ -79,41 +79,55 @@ var grays = []int{
 }
 
 type randomColor struct {
-	history []int
+	history [][]int
 }
 
-func (r *randomColor) isCollision(proposal, circleIndex int) bool {
-	globalIndex := len(r.history)
+func (r *randomColor) isCollision(proposal, circleIndex int, prevCircleNeighbor float64) bool {
 	if proposal == -1 {
 		return true
 	}
-	if globalIndex == 0 {
+	if circleIndex == 0 {
 		return false
 	}
-	if circleIndex == 1 && proposal == r.history[0] {
+	if circleIndex == 1 && proposal == r.history[0][0] {
 		return true
 	}
+	currentIndex := len(r.history[circleIndex])
 	circleSize := circleIndex * 6
-	indexToCheck := globalIndex - (circleSize - 1)
-	if indexToCheck > 0 && proposal == r.history[indexToCheck] {
+	if currentIndex+1 == circleSize && proposal == r.history[circleIndex][0] {
 		return true
 	}
-	if proposal == r.history[globalIndex-1] {
+	if currentIndex > 0 && proposal == r.history[circleIndex][currentIndex-1] {
+		return true
+	}
+	neighborRight := int(math.Floor(prevCircleNeighbor)) % len(r.history[circleIndex-1])
+	if proposal == r.history[circleIndex-1][neighborRight] {
+		return true
+	}
+	neighborLeft := int(math.Ceil(prevCircleNeighbor)) % len(r.history[circleIndex-1])
+	if proposal == r.history[circleIndex-1][neighborLeft] {
 		return true
 	}
 	return false
 }
 
-func (r *randomColor) get(circleIndex int) int {
+func (r *randomColor) get(circleIndex, localIndex int) int {
+	if len(r.history) <= circleIndex {
+		r.history = append(r.history, []int{})
+	}
+
+	ratio := float64(circleIndex) / (float64(circleIndex) - 1)
+	prevCircleNeighbor := float64(localIndex) / ratio
+
 	newColorIndex := -1
-	for r.isCollision(newColorIndex, circleIndex) {
+	for r.isCollision(newColorIndex, circleIndex, prevCircleNeighbor) {
 		newIndexBig, err := rand.Int(rand.Reader, big.NewInt(int64(len(grays))))
 		newColorIndex = int(newIndexBig.Int64())
 		if err != nil {
 			panic(err)
 		}
 	}
-	r.history = append(r.history, newColorIndex)
+	r.history[circleIndex] = append(r.history[circleIndex], newColorIndex)
 	return grays[newColorIndex]
 }
 
@@ -135,7 +149,7 @@ func (gen *HexagonGenerator) drawSingle(pos Point, color int) Hexagon {
 func (gen *HexagonGenerator) drawMultipleHexagons(M Point, rounds int) {
 	colorGen := randomColor{}
 
-	color := colorGen.get(0)
+	color := colorGen.get(0, 0)
 	firstH := gen.drawSingle(M, color)
 
 	hexagonWidth := float64(firstH.R) * 2 * math.Cos(math.Pi/6)
@@ -143,7 +157,7 @@ func (gen *HexagonGenerator) drawMultipleHexagons(M Point, rounds int) {
 	for c := 1; c < rounds; c++ {
 		hexInCircleCount := 6 * c
 
-		color := colorGen.get(c)
+		color := colorGen.get(c, 0)
 		h := gen.drawSingle(Point{
 			X: firstH.C.X,
 			Y: firstH.C.Y + firstH.R,
@@ -152,7 +166,7 @@ func (gen *HexagonGenerator) drawMultipleHexagons(M Point, rounds int) {
 
 		deg := math.Pi // 180°
 		for i := 1; i < hexInCircleCount; i++ {
-			color := colorGen.get(c)
+			color := colorGen.get(c, i)
 			h = gen.drawSingle(Point{
 				X: h.M.X + int(hexagonWidth*math.Cos(deg)),
 				Y: h.M.Y + int(hexagonWidth*math.Sin(deg)),
